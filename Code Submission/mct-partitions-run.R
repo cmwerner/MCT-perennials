ec0267e8caf006c3134b7b11a8cffc9e1679c442
Chhaya Werner <cwerner@ucdavis.edu>
2023-08-17 16:44:09 -0700

    Cleaning up unused code and preparing for submission

--- Code Submission/mct-partitions-run.R
+++ Code Submission/mct-partitions-run.R
@@ -10,24 +10,21 @@ library(dplyr)
 library(tidyr)
 library(tibble)
 
-# CHOOSE: This determines which coexistence pair we're looking at
+# This determines which coexistence pair we're looking at
 # case <- 'as'
  case <- 'p'
 
 pair <- paste(case, 'an', sep = '.')
 
-# varying <- 'lambda'
- varying <- 'alpha'
+varying <- 'alpha'
 
-# favorable <- 'same'
-# favorable <- 'opposite'
+# This determines which environmental scenario we're running
+ favorable <- 'opposite-consistent'
 # favorable <- 'same-consistent'
-# favorable <- 'opposite-consistent'
 # favorable <- 'same-flipped'
- favorable <- 'opposite-flipped'
 
-# file names
-
+ 
+# FILE NAMES
 file.core <- paste0(case, '_', varying, '-', favorable)
 main.wd <- '/project/coexistence/cwerner5/'
 
@@ -57,9 +54,8 @@ pars <- pars %>% mutate(seed.surv = NULL)
 time.warm.up <- 200
 time.full <- 300
 
-# WRITE A FUNCTION TO PASS TO THE WORKER NODES. The function should perform
-#    a single simulation, and will then be repeated enough times to produce
-#    the desired number of simulations.
+# FUNCTION TO PASS TO THE WORKER NODES. 
+# This function performs a single simulation and will be repeated
 
 Run_part_function <- function(i){
   
@@ -83,14 +79,13 @@ Run_part_function <- function(i){
     pars.2 <- pars.2 %>% full_join(pars.ave)
     
     
-    env.draw <- rbinom(time.full, 1, env.ratio) # sequence of the environment (NOTE: would want this to change if doing multiple runs)
+    env.draw <- rbinom(time.full, 1, env.ratio) # sequence of the environment
     env.condition <- if_else(env.draw > 0, 'wet','dry')
     
     partitions <- partition_epsilons(sp.inv = sp.2, sp.res = sp.1, pars.2, 
                                      env.condition, time.warm.up, time.full) %>%
       rbind(partition_epsilons(sp.inv = sp.1, sp.res = sp.2, pars.2, 
                                env.condition, time.warm.up, time.full)) %>%
-      #pivot_longer(cols = inv.e0:res.eint) %>%
       pivot_longer(cols = inv.e0:res.storage) %>%
       separate(name, into = c('player', 'partition')) %>%
       pivot_wider(names_from = player, values_from = value) %>%
@@ -109,32 +104,23 @@ Run_part_function <- function(i){
 # CREATE A CLUSTER AND RUN THE SIMULATIONS
 cl <- makeCluster(n_processors - 1, type = "MPI")
 
-# EXPORT ANY NECESSARY R OBJECTS from the preprocessing stage to the indivdidual
-#    processors (e.g. Do the processors need to know the value of any of the parameters
-#    set up before?). The names of the objects need to be stored together as character
-#    vectors because you are basically just telling R what to look for in the environment.
+# EXPORT NECESSARY R OBJECTS from the preprocessing stage to the indivdidual processors 
 
-ObjectsToImport <- c("pars", "time.warm.up", "time.full", "pair", "sp.1", "sp.2", "sp.order") # Check that this is everything
-
+ObjectsToImport <- c("pars", "time.warm.up", "time.full", "pair", "sp.1", "sp.2", "sp.order") 
 clusterExport(cl, ObjectsToImport) # values and vectors not functions
 
-# Run any commands necessary on the processors before running the simulation. This 
-#    is where you can set the working directory of the processors, source any
-#    necessary files, or load any necessary libraries
+# Run commands necessary on the processors before running the simulation, 
+# including setting the working directory of the processors, sourcing necessary files, 
+# and loading necessary libraries
 clusterEvalQ(cl, setwd("/project/coexistence/cwerner5/")) 
 clusterEvalQ(cl, library(dplyr))
 clusterEvalQ(cl, library(tidyr))
 clusterEvalQ(cl, library(tibble))
 clusterEvalQ(cl, source("mct-partitions-functions.R")) # put the functions used on each node
 
-# Run the simulations on the cluster. Here x corresponds to the first argument
-#    of the SimFunc function. The cluster will evaluate the SimFunc for each element
-#    of the vector x below. I usually just use a vector of 1 to the number of simulations,
-#    but you could get creative with this too, if you wanted. The output of the SimFunc
-#    function is stored in a list which you can immediately save, or process within this
-#    same script.
-simulations <- clusterApply(cl, x = 1:n_sims, fun = Run_part_function) # run it x amount of times
-
+# Run the simulations on the cluster n_sims times. 
+# The cluster will evaluate the SimFunc for each element of the vector x below. 
+simulations <- clusterApply(cl, x = 1:n_sims, fun = Run_part_function) 
 
 # Save as an r dataframe
 save(simulations, file=file.out)
